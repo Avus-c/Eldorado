e6838033b97c1e64419bbf813010a2ce669ae1b3
Andreas Lay <andreas.lay@sodgeit.de>
2024-07-10 19:14:17 +0200

    wip: something resembling a shop

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,86 @@
-#include <print>
 #include <raylib.h>
+
+const int screenWidth  = 1800;
+const int screenHeight = 1200;
+
+void DrawShop()
+{
+	int tmp = 155;
+	DrawRectangle( 100 + tmp, screenHeight - 400, 200, 300, GRAY );
+	DrawRectangle( 305 + tmp, screenHeight - 400, 200, 300, GRAY );
+	DrawRectangle( 510 + tmp, screenHeight - 400, 200, 300, GRAY );
+	DrawRectangle( 715 + tmp, screenHeight - 400, 200, 300, GRAY );
+	DrawRectangle( 920 + tmp, screenHeight - 400, 200, 300, GRAY );
+	DrawRectangle( 1125 + tmp, screenHeight - 400, 200, 300, GRAY );
+
+	DrawRectangle( 50, screenHeight - 420, 1700, 15, RED );
+
+	DrawRectangle( 100 + tmp, screenHeight - 725, 200, 300, GRAY );
+	DrawRectangle( 305 + tmp, screenHeight - 725, 200, 300, GRAY );
+	DrawRectangle( 510 + tmp, screenHeight - 725, 200, 300, GRAY );
+	DrawRectangle( 715 + tmp, screenHeight - 725, 200, 300, GRAY );
+	DrawRectangle( 920 + tmp, screenHeight - 725, 200, 300, GRAY );
+	DrawRectangle( 1125 + tmp, screenHeight - 725, 200, 300, GRAY );
+
+	DrawRectangle( 100 + tmp, screenHeight - 1050, 200, 300, GRAY );
+	DrawRectangle( 305 + tmp, screenHeight - 1050, 200, 300, GRAY );
+	DrawRectangle( 510 + tmp, screenHeight - 1050, 200, 300, GRAY );
+	DrawRectangle( 715 + tmp, screenHeight - 1050, 200, 300, GRAY );
+	DrawRectangle( 920 + tmp, screenHeight - 1050, 200, 300, GRAY );
+	DrawRectangle( 1125 + tmp, screenHeight - 1050, 200, 300, GRAY );
+}
 
 int main()
 {
-	std::print( "Welcome to eldorado" );
+	InitWindow( screenWidth, screenHeight, "Eldorado" );
 
-	InitWindow( 800, 400, "Eldorado" );
+	bool      shopOpened = false;
+	bool      turnActive = false;
+	Rectangle turnBox{ screenWidth - 152, 2, 150, 30 };
+	Color     turnBoxColor = DARKGRAY;
+	Rectangle shopBox{ screenWidth - 152 - 152, 2, 150, 30 };
+	Color     shopBoxColor = DARKGREEN;
+
+	SetTargetFPS( 30 );
 
 	while ( !WindowShouldClose() )
 	{
+		Vector2 mousePoint = GetMousePosition();
+		if ( CheckCollisionPointRec( mousePoint, turnBox ) && IsMouseButtonPressed( MOUSE_LEFT_BUTTON ) )
+		{
+			turnActive = !turnActive;
+		}
+
+		if ( CheckCollisionPointRec( mousePoint, shopBox ) && IsMouseButtonPressed( MOUSE_LEFT_BUTTON ) )
+		{
+			shopOpened = !shopOpened;
+		}
+
 		BeginDrawing();
 		ClearBackground( RAYWHITE );
-		DrawText( "Eldorado", 200, 200, 20, BLACK );
-		DrawPoly( ( Vector2 ){ 160 / 4.0f * 2, 210 }, 6, 80, 30, BROWN );
+
+		DrawRectangleRec( turnBox, turnBoxColor );
+		DrawRectangleRec( shopBox, shopBoxColor );
+
+		if ( turnActive )
+		{
+			DrawText( "Finish Turn!", screenWidth - 145, 7, 20, RED );
+		}
+		else
+		{
+			DrawText( "Wait Opponent", screenWidth - 145, 7, 20, WHITE );
+		}
+
+		if ( shopOpened )
+		{
+			DrawText( "Exit Shop", screenWidth - 145 - 145, 7, 20, WHITE );
+			DrawShop();
+		}
+		else
+		{
+			DrawText( "Open Shop", screenWidth - 145 - 145, 7, 20, WHITE );
+		}
+
 		EndDrawing();
 	}
 
